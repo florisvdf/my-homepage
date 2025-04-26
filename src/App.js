36b1d266ee0392c859886d2ad3f2d3e97439c088
Floris vanderFlier <floris.vanderflier@iff.com>
2025-04-26 21:10:05 +0200

    added bio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
+import bio from './content/bio';
+
+
 function App() {
   return (
     <div style={{ maxWidth: 600, margin: "auto", padding: 20, fontFamily: "Arial, sans-serif" }}>
       <h1>Floris van der Flier</h1>
-      <img src="me.jpg" alt="Avatar" style={{ width: 150, borderRadius: '50%' }} />
-      <p>Hello! This is my personal homepage.</p>
+      <img src="/me.jpg" alt="Avatar" style={{ width: 150, borderRadius: '50%' }} />      
+      <h1>About Me</h1>
+      <p>{bio}</p>
       <a href="cv.pdf" target="_blank" rel="noopener noreferrer">
         View My CV
       </a>
