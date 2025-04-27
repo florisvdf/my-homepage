8641769067f9986722cd2e08c254bde8eb151f7f
florisvdf <38347914+florisvdf@users.noreply.github.com>
2025-04-27 11:23:08 +0200

    Feature/bio (#1)
    * added bio
    
    * removed backslash
    
    * feat: added bio as .md file
    
    ---------
    
    Co-authored-by: Floris vanderFlier <floris.vanderflier@iff.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+// import bio from './content/bio';
+
+
 function App() {
+  const [bio, setBio] = useState("");
+
+  useEffect(() => {
+    fetch(process.env.PUBLIC_URL + "/bio.md")
+      .then((res) => res.text())
+      .then((text) => setBio(text));
+  }, []);
+
   return (
     <div style={{ maxWidth: 600, margin: "auto", padding: 20, fontFamily: "Arial, sans-serif" }}>
       <h1>Floris van der Flier</h1>
-      <img src="me.jpg" alt="Avatar" style={{ width: 150, borderRadius: '50%' }} />
-      <p>Hello! This is my personal homepage.</p>
-      <a href="cv.pdf" target="_blank" rel="noopener noreferrer">
-        View My CV
-      </a>
+      <img src={process.env.PUBLIC_URL + '/me.jpg'} alt="Avatar" style={{ width: 150, borderRadius: '50%' }} />
+      <h1>About Me</h1>
+      <ReactMarkdown>{bio}</ReactMarkdown>
+      {/* <p>{bio}</p> */}
+      <a href={process.env.PUBLIC_URL + '/cv.pdf'} target="_blank" rel="noopener noreferrer">View My CV</a>
     </div>
   );
 }
