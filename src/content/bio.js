2245318bef08ed95f5d9c9d7e140a3c2a6b10512
Floris vanderFlier <floris.vanderflier@iff.com>
2025-04-27 11:20:27 +0200

    feat: added bio as .md file

--- src/content/bio.js
+++
@@ -1,7 +0,0 @@
-const bio = `
-I am a PhD student at the Bioinformatics group of Wageningen University supervised by Dick de Ridder and work closely together with IFF under the co-supervision of Henning Redestig. For many years, I have been fascinated by the complexity and beauty of proteins, and their potential to perform versatile chemistry. I am particularly interested in how we can unlock this potential and create new proteins that can provide more sustainable alternative to existing industrial processes.\n
-
-The focal point of my PhD lies in using optimization algorithms to identify protein variants of existing proteins with improved desired properties. I am currently working on informing machine learning models with physical priors to improve prediction accuracy, and also on uncertainty quantification methods to better navigate the search space of variant sequences.
-`;
-
-export default bio;
